b8775bb7365b9c5dfe7cc0751840779d8b13fa2e
sibiu <razvantrufin99x@gmail.com>
2025-11-07 19:11:46 +0200

    Add project files.

---
+++ coalzConjusture/MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 11/7/2025
+ * Time: 6:40 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace coalzConjusture
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(858, 56);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(816, 568);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(943, 12);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(862, 13);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Colaz";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(781, 13);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "ColazDraw";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(1049, 9);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "reset";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1751, 636);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "coalzConjusture";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ coalzConjusture/MainForm.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 11/7/2025
+ * Time: 6:40 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace coalzConjusture
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+		
+		public Graphics g;
+		public Pen p = new Pen(Color.Red,2);
+		public Font f;
+		public Brush b = new SolidBrush(Color.Black);
+		
+		
+		
+		public List<int>cdate = new List<int>();
+		public void cc(int n)
+		{
+		
+			int counter = 0;
+            while (n != 1)
+            {
+            	counter++;
+            	textBox1.Text += counter.ToString() + " : " + n.ToString() + "\r\n";
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                }
+                cdate.Add(n);
+            }
+            textBox1.Text += "1";
+            cdate.Add(1);
+       
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			g = this.CreateGraphics();
+			f = this.Font;
+			textBox2.Text = "1000";
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			cc(int.Parse(this.textBox2.Text));
+		}
+		public void grafic()
+		{
+			for(int i = 0 ; i < cdate.Count ; i++)
+			{
+				try{
+					
+					g.DrawLine(p,100+(i*5),cdate[i]+50,100+(i*5),cdate[i-1]+50);
+				g.DrawEllipse(p,100+(i*5),cdate[i]+50,5,5);
+				g.DrawString(cdate[i].ToString(),f,b,(i*5)+100,cdate[i]+50+50);
+				}catch{};
+			}
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			grafic();
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			cdate.Clear();
+			g.Clear(this.BackColor);
+		}
+	}
+}
+
+
